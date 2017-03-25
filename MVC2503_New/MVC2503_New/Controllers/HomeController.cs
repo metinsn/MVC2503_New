f8e67e5031a8f23a8b8014282103ec22ae24ca9c
metin.sen <metin.sen@ALS-YG39506-J06>
2017-03-25 17:58:04 +0300

    1

--- MVC2503_New/MVC2503_New/Controllers/HomeController.cs
+++ MVC2503_New/MVC2503_New/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC2503_New.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,23 @@ namespace MVC2503_New.Controllers
         {
             ViewData["Mesaj 1"] = "Bu bir Viewdata 1 mesajıdır";
             ViewData["Mesaj 2"] = "Bu bir Viewdata 2 mesajıdır";
+            ViewBag.Mesaj = "Bu bir ViewBag Mesajıdır.";
+            ViewBag.Bilgeadam = "Bilge Kadın";
+
+            TempData["metin"]= "Index Actiondan Geliyorum :)";
+            //return View();
+            return RedirectToAction("index2");
+        }
+        public ActionResult index2()
+        {
+            return View();
+        }
+        public ActionResult index3()
+        {
+            List<Users> kullaniciListesi = new List<Users>();
+            kullaniciListesi.AddRange(new List<Users> { new Users() { adi="Yavuz",soyadi="Gedik"}, new Users() { adi = "Seçkin", soyadi = "Umur" }, new Users() { adi = "Özgür", soyadi = "Saymaz" } });
+            ViewBag.kullanicilar = kullaniciListesi;
+
             return View();
         }
     }
---
+++ MVC2503_New/MVC2503_New/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC2503_New.Models
+{
+    public class Users
+    {
+        public string adi { get; set; }
+        public string soyadi { get; set; }
+    }
+}
